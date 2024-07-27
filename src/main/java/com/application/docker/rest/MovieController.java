89a424c2683709d592eecd3608c2395619cb532f
harisha3193 <harisha3193@gmail.com>
2024-07-26 22:56:18 -0400

    modified 2

--- src/main/java/com/application/docker/rest/MovieController.java
+++ src/main/java/com/application/docker/rest/MovieController.java
@@ -3,6 +3,7 @@ package com.application.docker.rest;
 import com.application.docker.dto.MovieDto;
 import com.application.docker.persistence.model.Movie;
 import com.application.docker.service.IMovieService;
+//import com.application.docker.service.impl.GitHubArtifactUploader;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,10 +15,18 @@ public class MovieController {
 
   private IMovieService movieService;
 
+  //private GitHubArtifactUploader gitHubArtifactUploader;
+
   @PostMapping("api/v1/movie")
   public ResponseEntity get(@RequestBody final MovieDto movieDto) {
     Movie movie = getMovieToCreate(movieDto);
     Movie createdMovie = movieService.create(movie);
+
+    try {
+     // gitHubArtifactUploader.uploadArtifact();
+    } catch(Exception ex){
+      System.out.println("Errorrr");
+    }
     return ResponseEntity.ok(MovieDto.builder()
         .movieName(createdMovie.getMovieName())
         .category(createdMovie.getCategory())
--- src/main/java/com/application/docker/service/impl/GitHubArtifactUploader.java
+++ src/main/java/com/application/docker/service/impl/GitHubArtifactUploader.java
@@ -1,4 +1,47 @@
+/*
 package com.application.docker.service.impl;
 
-public class test {
+import org.apache.http.HttpHeaders;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.IOException;
+
+@Service
+public class GitHubArtifactUploader {
+
+    private static final String GITHUB_API_ENDPOINT = "https://api.github.com";
+    private static final String OWNER = "harisha3193";
+    private static final String REPO_NAME = "docker-example";
+    private static final String RELEASES_URL = GITHUB_API_ENDPOINT + "/repos/" + OWNER + "/" + REPO_NAME + "/releases";
+
+
+    public void uploadArtifact() throws IOException {
+        HttpClient httpClient = HttpClients.createDefault();
+        HttpPost httpPost = new HttpPost(RELEASES_URL);
+        httpPost.setHeader(HttpHeaders.AUTHORIZATION, "token " + "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");
+
+        MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+
+        File artifactFile = new File("");
+
+        builder.addBinaryBody("file", artifactFile);
+
+        httpPost.setEntity(builder.build());
+
+        HttpResponse response = httpClient.execute(httpPost);
+        int statusCode = response.getStatusLine().getStatusCode();
+
+        if (statusCode == 201) {
+            System.out.println("Artifact uploaded successfully!");
+        } else {
+            System.err.println("Failed to upload artifact. Status code: " + statusCode);
+        }
+    }
 }
+*/
